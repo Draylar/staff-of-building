2a21f4868c53d94a1b39bd709053862bd5415429
Draylar <samuelhimes@gmail.com>
2021-04-21 16:28:54 -0500

    netherite staff no longer burns in lava

--- src/main/java/draylar/staffofbuilding/registry/ModItems.java
+++ src/main/java/draylar/staffofbuilding/registry/ModItems.java
@@ -15,7 +15,7 @@ public class ModItems {
     public static final Item IRON_BUILDER_STAFF = register("iron_builder_staff", new BuilderStaffItem(new Item.Settings().group(StaffOfBuilding.GROUP).maxDamage(500), CONFIG.ironSize, Items.IRON_INGOT)); // +12
     public static final Item GOLDEN_BUILDER_STAFF = register("golden_builder_staff", new BuilderStaffItem(new Item.Settings().group(StaffOfBuilding.GROUP).maxDamage(150), CONFIG.goldenSize, Items.GOLD_INGOT)); // +16
     public static final Item DIAMOND_BUILDER_STAFF = register("diamond_builder_staff", new BuilderStaffItem(new Item.Settings().group(StaffOfBuilding.GROUP).maxDamage(1000), CONFIG.diamondSize, Items.DIAMOND)); // +16
-    public static final Item NETHERITE_BUILDER_STAFF = register("netherite_builder_staff", new BuilderStaffItem(new Item.Settings().group(StaffOfBuilding.GROUP).maxDamage(1250), CONFIG.netheriteSize, Items.NETHERITE_INGOT)); // +20
+    public static final Item NETHERITE_BUILDER_STAFF = register("netherite_builder_staff", new BuilderStaffItem(new Item.Settings().group(StaffOfBuilding.GROUP).maxDamage(1250).fireproof(), CONFIG.netheriteSize, Items.NETHERITE_INGOT)); // +20
     public static final Item INFINITE_BUILDER_STAFF = register("infinite_builder_staff", new BuilderStaffItem(new Item.Settings().group(StaffOfBuilding.GROUP), CONFIG.infiniteSize, null)); // + 24 + 28 + 32
 
     private static <T extends Item> T register(String name, T item) {
