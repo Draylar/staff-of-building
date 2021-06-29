8b1b0edd04db7100c22f43a076230d568eef9045
Draylar <17728338+Draylar@users.noreply.github.com>
2021-06-28 19:59:28 -0500

    item group to tools - haykam would approve

--- src/main/java/draylar/staffofbuilding/StaffOfBuilding.java
+++ src/main/java/draylar/staffofbuilding/StaffOfBuilding.java
@@ -1,15 +1,11 @@
 package draylar.staffofbuilding;
 
+import draylar.omegaconfig.OmegaConfig;
 import draylar.staffofbuilding.config.StaffOfBuildingConfig;
 import draylar.staffofbuilding.registry.ModItems;
-import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
-import me.sargunvohra.mcmods.autoconfig1u.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 import java.util.Arrays;
@@ -17,8 +13,7 @@ import java.util.List;
 
 public class StaffOfBuilding implements ModInitializer {
 
-    public static final ItemGroup GROUP = FabricItemGroupBuilder.build(id("group"), () -> new ItemStack(ModItems.STONE_BUILDER_STAFF));
-    public static final StaffOfBuildingConfig CONFIG = AutoConfig.register(StaffOfBuildingConfig.class, JanksonConfigSerializer::new).getConfig();
+    public static final StaffOfBuildingConfig CONFIG = OmegaConfig.register(StaffOfBuildingConfig.class);
     public static final List<Block> RESET_LIST = Arrays.asList(Blocks.SNOW, Blocks.COMPOSTER, Blocks.CAULDRON, Blocks.CAKE, Blocks.BEEHIVE, Blocks.BREWING_STAND);
 
     @Override
